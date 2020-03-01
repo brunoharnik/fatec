61a38c037eab0f35aa3a67a349102ca5ed3f0167
Bruno Harnik <brunoharnik@gmail.com>
2020-03-01 19:22:04 -0300

    Cobol

--- 4SEM/ILP007-Programação Orientada a Objetos/Algoritmos feitos em casa/Estouro de memória heap/src/EstourarMemoriaHeap.java
+++ 4SEM/ILP007-Programação Orientada a Objetos/Algoritmos feitos em casa/Estouro de memória heap/src/EstourarMemoriaHeap.java
@@ -1,7 +1,7 @@
 public class EstourarMemoriaHeap {
 	
 	EstourarMemoriaHeap proximo;
-	int vetorGrande[] = new int[100000];
+	int vetorGrande[] = new int[50000];
 
 	public static void main(String[] args) throws OutOfMemoryError {
 		// TODO Auto-generated method stub
